5776dffebf969c6f99f0019f5911f87f78182322
joker <417004794@qq.com>
2019-10-19 09:54:03 +0800

    更新脚本..

--- close-port-process.bat
+++ close-port-process.bat
@@ -1,46 +1,54 @@
 @echo off
-set /p port=请输入要关闭端口:
+echo **********************************************************
+echo     	     关闭端口脚本 by joker-pper
+echo **********************************************************
 
-set file_result=port-result-file
+set /p CLOSE_PORT=请输入要关闭的端口:
 
-netstat -nao | findstr %port%>%file_result%
+::设置结果输出的文件
+set RESULT_OUT_FILE=CLOSE_PORT_RESULT_FILE
+
+netstat -nao | findstr %CLOSE_PORT%>%RESULT_OUT_FILE%
 
 setlocal enabledelayedexpansion
 set index=0
 set closed_pid=-1
-for /f "tokens=1,2,3,4,5 delims= " %%a in (!file_result!) do (
+for /f "tokens=1,2,3,4,5 delims= " %%a in (!RESULT_OUT_FILE!) do (
 	set treaty=%%a
 	set local_address=%%b
 	set out_address=%%c
 	set status=%%d
 	set pid=%%e
 
-	set /a index=index+1
 	set key=!treaty! - !local_address! - !out_address! - !status! - !pid!
 
-	echo !key!
+	::echo !key!
 	if !status! == LISTENING (
+	        ::仅在LISTENING下增加索引值
+		set /a index=index+1
+
 		if !closed_pid! == !pid! (
 			echo 已关闭pid - !pid!进程
 
 		) else (
-
-			set /p close=是否关闭【0:否, 1:是】:
+			echo ----------------------------------------------------------
+			echo !key!
+			echo ----------------------------------------------------------
+			set /p close=是否关闭 【0:否, 1:是】:
 			if !close! == 1 (
 				taskkill -pid !pid! -f
 				set closed_pid=!pid!
 				echo 关闭pid - !pid!进程
 			)
 		)
-	) else (
-		echo 当前跳过关闭
 	)
-)
 
-del %file_result%
+)
+::删除输出文件
+del %RESULT_OUT_FILE%
 
 if %index% == 0 (
-	echo 未找到相关信息
+	echo 未找到该端口的相关信息
 )
 
 pause
--- md5.bat
+++ md5.bat
@@ -1,23 +1,26 @@
 @echo off
-echo ******获取文件MD5******
+echo ****************************************
+echo     获取文件MD5脚本 by joker-pper
+echo ****************************************
 
-set /p APP_FILE=请输入文件路径:
+set /p FILE_FULL_NAME=请输入文件路径:
+
 
 ::获取md5值
-certutil -hashfile %APP_FILE% MD5 >APP_MD5_TXT
+certutil -hashfile %FILE_FULL_NAME% MD5 >FILE_MD5_TXT
 setlocal enabledelayedexpansion
 set index=0
-for /f "tokens=* delims= " %%i in (APP_MD5_TXT) do (
+for /f "tokens=* delims= " %%i in (FILE_MD5_TXT) do (
 	::echo %%i
-	if !index! == 1 (set md5=%%i)
+	if !index! == 1 (set FILE_MD5=%%i)
  	set /a index=index+1
 )
 
-del APP_MD5_TXT
+del FILE_MD5_TXT
 
 REM 移除md5中的空格,兼容md5出现空格的情况
-set md5=%md5: =%
+set FILE_MD5=%FILE_MD5: =%
 
-echo 当前文件MD5: %md5%
+echo 当前文件(%FILE_FULL_NAME%)-MD5: %FILE_MD5%
+
 pause
-
