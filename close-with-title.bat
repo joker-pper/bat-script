3c5a35f6adca72e9968f366a03d9d6cb638cc6ae
joker-pper <417004794@qq.com>
2019-10-20 16:40:45 +0800

    Update close-with-title.bat

--- close-with-title.bat
+++ close-with-title.bat
@@ -3,8 +3,10 @@
 echo **********************************************************
 echo     	              关闭带标题的脚本
 echo **********************************************************
-
-
 set cmd_title=first-title-cmd
-taskkill /f /fi "windowtitle eq %cmd_title%
+taskkill /f /fi "windowtitle eq %cmd_title%"
+
+
+
+
 
