6fdc784e86f18a1850917ffae40df85f911115c5
joker <417004794@qq.com>
2019-10-20 16:46:42 +0800

    update

--- close-with-title.bat
+++ close-with-title.bat
@@ -1,10 +1,10 @@
 @echo off
 
 echo **********************************************************
-echo     	              关闭带标题的脚本
+echo     	     close the first title cmd
 echo **********************************************************
 
 set cmd_title=first-title-cmd
 
 taskkill /f /fi "windowtitle eq %cmd_title%"
-pause
+::pause
